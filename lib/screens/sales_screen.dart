a0706996e76f703052c41f9a9fd497c898642bb2
Muhammad-Arham383 <muhammadarhambajwa1@gmail.com>
2025-01-26 01:59:06 +0500

    recommit

--- lib/screens/sales_screen.dart
+++ lib/screens/sales_screen.dart
@@ -46,10 +46,6 @@ class _SalesScreenState extends State<SalesScreen> {
     var width = MediaQuery.of(context).size.width;
     var height = MediaQuery.of(context).size.height;
     return Scaffold(
-        floatingActionButton: FloatingActionButton(onPressed: () {
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (builder) => SalesScreen()));
-        }),
         backgroundColor: Colors.lightBlue.shade200,
         appBar: AppBar(
           backgroundColor: const Color.fromARGB(255, 41, 121, 255),
@@ -63,118 +59,79 @@ class _SalesScreenState extends State<SalesScreen> {
         ),
         body: SingleChildScrollView(
           child: Align(
-            alignment: Alignment.center,
-            child: BlocBuilder<InventoryBloc, InventoryState>(
-              builder: (context, state) {
-                if (state is InventoryLoadingState) {
-                  return const CircularProgressIndicator();
-                } else if (state is InventoryErrorState) {
-                  return Text(
-                    'Error: ${state.errorMessage}',
-                    style: const TextStyle(color: Colors.red),
-                  );
-                } else if (state is ProductByNameLoadedState) {
-                  return Row(
-                    spacing: 15,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Column(
-                        children: [
-                          FittedBox(
-                            child: Text(
-                              'Product: ${state.product.productName}',
-                              style: const TextStyle(
-                                  fontSize: 30, fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          FittedBox(
-                            child: Text(
-                              'Price per Unit: \$${state.product.price}',
-                              style: const TextStyle(fontSize: 20),
-                            ),
-                          )
-                        ],
-                      )
+              alignment: Alignment.center,
+              child: Container(
+                margin: EdgeInsets.only(top: height * 0.06),
+                width: width * 0.9,
+                height: height * 0.7,
+                decoration: const BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.white,
+                          offset: Offset(5, 5),
+                          blurRadius: 7),
+                      BoxShadow(
+                          color: Colors.lightBlue,
+                          offset: Offset(-8, -8),
+                          blurRadius: 20)
                     ],
-                  );
-                } else {
-                  return Container(
-                    margin: EdgeInsets.only(top: height * 0.06),
-                    width: width * 0.9,
-                    height: height * 0.7,
-                    decoration: const BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.white,
-                              offset: Offset(5, 5),
-                              blurRadius: 7),
-                          BoxShadow(
-                              color: Colors.lightBlue,
-                              offset: Offset(-8, -8),
-                              blurRadius: 20)
-                        ],
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(12),
-                        ),
-                        color: Color.fromARGB(255, 161, 213, 237)),
-                    child: Column(
-                      spacing: 10,
-                      children: [
-                        Container(
-                          width: width * 0.7,
-                          height: height * 0.06,
-                          decoration: const BoxDecoration(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(12),
-                              ),
-                              color: Color.fromARGB(255, 41, 121, 255)),
-                          child: const FittedBox(
-                            child: Text(
-                              "Purchase Order",
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white),
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          width: width * 0.6,
-                          child: TextFormField(
-                            controller: _productNameController,
-                            decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                hintText: 'Product Name'),
-                          ),
-                        ),
-                        SizedBox(
-                          width: width * 0.6,
-                          child: TextFormField(
-                            onChanged: (value) {
-                              final quantity = int.tryParse(value) ?? 0;
-
-                              context.read<InventoryBloc>().add(
-                                    UpdateQuantityEvent(quantity: quantity),
-                                  );
-                            },
-                            keyboardType: TextInputType.number,
-                            controller: _quantityController,
-                            decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                hintText: 'Quantity'),
-                          ),
-                        ),
-                        ElevatedButton(
-                          onPressed: _onProductNameChanged,
-                          child: const Text('Search Product'),
-                        ),
-                      ],
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(12),
                     ),
-                  );
-                }
-              },
-            ),
-          ),
+                    color: Color.fromARGB(255, 161, 213, 237)),
+                child: Column(
+                  spacing: 10,
+                  children: [
+                    Container(
+                      width: width * 0.7,
+                      height: height * 0.06,
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(12),
+                          ),
+                          color: Color.fromARGB(255, 41, 121, 255)),
+                      child: const FittedBox(
+                        child: Text(
+                          "Purchase Order",
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: width * 0.6,
+                      child: TextFormField(
+                        controller: _productNameController,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Product Name'),
+                      ),
+                    ),
+                    SizedBox(
+                      width: width * 0.6,
+                      child: TextFormField(
+                        onChanged: (value) {
+                          final quantity = int.tryParse(value) ?? 0;
+
+                          context.read<InventoryBloc>().add(
+                                UpdateQuantityEvent(quantity: quantity),
+                              );
+                        },
+                        keyboardType: TextInputType.number,
+                        controller: _quantityController,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(), hintText: 'Quantity'),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: _onProductNameChanged,
+                      child: const Text('Search Product'),
+                    ),
+                  ],
+                ),
+              )),
         ));
   }
 }
